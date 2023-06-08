aa50c75e82ae371c52ac8441843511defb79ba84
louisehnrd <134003016+louisehnrd@users.noreply.github.com>
2023-06-08 13:24:29 +0200

    Update user_space.py

--- user_space.py
+++ user_space.py
@@ -1,6 +1,7 @@
 from picture import take_picture, suppr_picture, last_picture, param_zoom
 from flask import Flask, redirect, url_for, request, render_template, send_from_directory, make_response
 import os
+from time import sleep
 from picamera2 import Picamera2, Metadata
 from libcamera import controls
 
@@ -15,21 +16,22 @@ def choice():
     if request.method == 'POST':
         valeur_select = request.form['action']
         if valeur_select == 'Picture':
-            return redirect(url_for('picture'))
+            return redirect(url_for('param'))
         else :
            return redirect(url_for('video'))
     return render_template('form.html')
 
 
-@app.route('/Picture', methods=['POST', 'GET'])
-def picture():
+@app.route('/Param', methods=['POST', 'GET'])
+def param():
     #initialisation variable
-    global width, height, zoom_factor
+    global width, height, zoom_factor,lens_position
+    """
     width = False
     height = False
     zoom_factor = False
     lens_position = False
-
+"""
     if request.method == 'POST':
         if 'parameters' in request.form :
             #choice parameters & take picture
@@ -38,23 +40,34 @@ def picture():
             zoom_factor = request.form['zoom_factor']
             lens_position = request.form['lens_position']
             take_picture(width, height,zoom_factor,lens_position)
-            return redirect(url_for('picture'))
+            print(width)
+            return redirect(url_for('param'))
         
         elif 'choice' in request.form :
-            if width == False :
+            if width is None :
+                #à modifier pour s'assurer que ça ne fasse pas d'erreur
                 return "You didn't take a photo. Please choose the photo parameters and click on 'submit'."
             else : 
                 #display & choice keep picture
                 reponse = request.form['reponse']
+                #Deletes the photo and re-selects the settings
                 if reponse == 'non' :
                     path_file = last_picture()
                     suppr_picture(path_file)
-                    return redirect(url_for('picture'))
+                    return redirect(url_for('param'))
+                #send to picture page
                 else :
-                    print(width)
-                    return 'yes'
+                    return redirect(url_for('picture', width=width, height=height, zoom_factor=zoom_factor, lens_position=lens_position))
     
+    return render_template('param.html')
+
+@app.route('/Picture/<width>/<height>/<zoom_factor>/<lens_position>', methods=['POST', 'GET'])
+def picture(width,height,zoom_factor,lens_position):
+    if request.method == 'POST':
+        take_picture(width,height,zoom_factor,lens_position)
+        sleep(10)
     return render_template('picture.html')
+
 
 @app.route('/Video', methods=['POST', 'GET'])
 def video():
