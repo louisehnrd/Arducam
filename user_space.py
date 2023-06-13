cfdc70f132e641745b0d86dadbee3e3da16b4859
louisehnrd <134003016+louisehnrd@users.noreply.github.com>
2023-06-13 12:05:04 +0200

    Update user_space.py

--- user_space.py
+++ user_space.py
@@ -1,142 +1,97 @@
-<!doctype html>
+from picture import take_picture, suppr_picture, last_picture, param_zoom, update_config
+from flask import Flask, redirect, url_for, request, render_template, send_from_directory, make_response
+import os
+from time import sleep
+from picamera2 import Picamera2, Metadata
+from libcamera import controls
 
-<html>
-    <title>
-        Picture
-    </title>
-    <head>
-        <style>
-            h1 {
-                text-align: center;
-            }
+os.environ['LD_LIBRARY_PATH'] = '/usr/lib/arm-linux-gnueabihf/'
 
-            #parameters {
-                display: inline-block;
-                width: 50%; 
-                vertical-align: top; 
-                padding-left: 10%;
-            }
+app = Flask(__name__)
+IMG_FOLDER = os.path.join('static')
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+app.config["DEBUG"] = True
 
-            #choice {
-                display: inline-block;
-                width: 50%; 
-                vertical-align: top; 
-                padding-left: 10%;
-            }
+@app.route('/', methods=['POST', 'GET'])
+def choice():
+    if request.method == 'POST':
+        valeur_select = request.form['action']
+        if valeur_select == 'Picture':
+            return redirect(url_for('param'))
+        else :
+           return redirect(url_for('video'))
+    return render_template('form.html')
 
-            #picture {
-                float: right;
-                width: 50%;
-                margin-top: -35%;
-            }
 
-            #last_param {
-                float: right;
-                width: 50%;
-                margin-right: -60%;
-            }
+@app.route('/Param', methods=['POST', 'GET'])
+def param():
+    #variable initialization
+    global width, height, zoom_factor,lens_position
+    photo_recent = last_picture()
+    picture = os.path.join(app.config['UPLOAD_FOLDER'], photo_recent[-29:])
 
-            #condition{
-                font-size: 60%;
-            }
+    #Initialise default values
+    if 'width' not in globals():
+        width = None
+    if 'height' not in globals():
+        height = None
+    if 'zoom_factor' not in globals():
+        zoom_factor = None
+    if 'lens_position' not in globals():
+        lens_position = None
+    
+    if request.method == 'POST':
+        if 'parameters' in request.form :
+            #choice parameters & take picture
+            width = int(request.form['width'])
+            height = int(request.form['height'])
+            zoom_factor = request.form['zoom_factor']
+            lens_position = request.form['lens_position']
+            take_picture(width, height,zoom_factor,lens_position)
+            return redirect(url_for('param'))
+        
+        elif 'choice' in request.form :
+            if width is None :
+                #à modifier pour s'assurer que ça ne fasse pas d'erreur
+                return "You didn't take a photo. Please choose the photo parameters and click on 'submit'."
+            else : 
+                #display & choice keep picture
+                reponse = request.form['reponse']
+                #Deletes the photo and re-selects the settings
+                if reponse == 'non' :
+                    path_file = last_picture()
+                    suppr_picture(path_file)
+                    return redirect(url_for('param'))
+                #send to picture page
+                else :
+                    #writing to the config.json file
+                    on = True
+                    update_config(on,width,height,zoom_factor,lens_position)
+                    return redirect(url_for('picture'))
+    
+    if not width :
+        width = 'None'
+        height = 'None'
+        zoom_factor = 'None'
+        lens_position = 'None'
+    return render_template('param.html', user_image=picture, width=width, height=height, zoom_factor=zoom_factor, lens_position=lens_position)
 
-        </style>
-        <script>
-            // Fonction pour redimensionner une image
-            function resizeImage(image, newWidth, newHeight) {
-                // Création d'un élément <canvas>
-                var canvas = document.createElement('canvas');
-                canvas.width = newWidth;
-                canvas.height = newHeight;
-                
-                // Dessin de l'image sur le canvas avec la nouvelle taille
-                var context = canvas.getContext('2d');
-                context.drawImage(image, 0, 0, newWidth, newHeight);
-                
-                // Conversion du canvas en une nouvelle image
-                var resizedImage = new Image();
-                resizedImage.src = canvas.toDataURL('image/jpeg');
-                
-                // Retourne l'image redimensionnée
-                return resizedImage;
-            }
-              
-            // Fonction pour changer automatiquement la photo
-            function changerPhotoAutomatiquement() {
-                // Création de l'élément <img>
-                var image = document.createElement('img');
-                
-                // Définition de l'attribut src
-                image.src = "{{ user_image }}";
-                
-                // Redimensionnement de l'image à la taille souhaitée (640x480)
-                var newWidth = 640;
-                var newHeight = 480;
-                var resizedImage = resizeImage(image, newWidth, newHeight);
-                
-                // Suppression de l'ancienne image
-                var pictureElement = document.getElementById('picture');
-                while (pictureElement.firstChild) {
-                    pictureElement.removeChild(pictureElement.firstChild);
-                }
-                
-                // Ajout de la nouvelle image redimensionnée
-                pictureElement.appendChild(resizedImage);
-            }
-            
-            // Appel initial de la fonction pour changer la photo
-            changerPhotoAutomatiquement();
-            
-            // Appel de la fonction pour changer la photo toutes les 5 secondes
-            setInterval(changerPhotoAutomatiquement, 1000); // Change la photo toutes les 5 secondes (5000 millisecondes)
-        </script>
-    </head>
-    <body>
-        <h1>Configuration of the arducam 64mp</h1>
-        <div>                
-            <div id="parameters">
-                <form action="/Param" method="post">
-                    <h2>Choose the desired parameters</h2>
-                    <p>Enter the width :</p>
-                    <p>
-                        <input type = "text" name = "width" value="4624"/>                        </p>
-                    <p>Enter the height :</p>
-                    <p>
-                        <input type = "text" name = "height" value="3472"/>
-                    </p>
-                    <p>Enter zoom factor (1 to 10)</p>
-                    <p>
-                        <input type = "text" name = "zoom_factor" value="1"/>
-                    </p>
-                    <p>Enter lens position (0 to 15)*</p>
-                    <p>
-                        <input type = "text" name = "lens_position" value="None"/>
-                    </p>
-                    <p id="condition">*If you want autofocus, don't enter anything </p>
-                    <p>
-                        <input type="submit" name ="parameters" value="submit" />
-                    </p>
-                </form>
-            </div>
 
-            <div id="choice">    
-                <form action="/Param" method="post">
-                    <h2> Do you want to keep this configuration ? </h2>
-                    <select name="reponse">                                    
-                        <option value="oui">oui</option>
-                        <option value="non">non</option>
-                    </select>
-                    <input type="submit" name="choice" value="submit">
-                </form>
-            </div>
+@app.route('/Picture', methods=['POST', 'GET'])
+def picture():
+    photo_recent = last_picture()
+    picture = os.path.join(app.config['UPLOAD_FOLDER'], photo_recent[-29:])
+    if request.method == 'POST':
+        if 'change' in request.form:
+            update_config(False,'null','null','null','null')
+            return redirect(url_for('param'))
+    return render_template('picture.html',user_image=picture) 
 
-            <div id="picture">
-            </div>
 
-            <div id="last_param">
-                <p> width : {{ width }}, height : {{height}}, zoom : {{zoom_factor}}, lens position : {{lens_position}}</p>
-            </div>
-        </div>
-              
-    </body>
-</html>
+@app.route('/Video', methods=['POST', 'GET'])
+def video():
+    return 'yes'
+
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=5000)
